6cf090bf05b8928f2a48e3a1a66bbc53cb5d4eef
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-21 14:10:53 +0300

    Formatting strings

--- main.go
+++ main.go
@@ -9,12 +9,11 @@ func main() {
 	const bmiPower = 2
 	var userHeight float64
 	var userWeight float64
-	fmt.Print("__ Калькулятор индекса массы тела __\n")
-	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Println("__ Калькулятор индекса массы тела __")
+	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userWeight)
-	bmi := userWeight / math.Pow(userHeight, bmiPower)
-	fmt.Print("Ваш индекс массы тела: ")
-	fmt.Print(bmi)
+	bmi := userWeight / math.Pow(userHeight / 100, bmiPower) 
+	fmt.Printf("Ваш индекс массы тела: %.0f", bmi)
 }
