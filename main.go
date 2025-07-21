067b06ded148f667b7332ff19afddc44f19a1bf7
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-21 13:31:39 +0300

    Input & output strings

--- main.go
+++ main.go
@@ -7,8 +7,14 @@ import (
 
 func main() {
 	const bmiPower = 2
-	userHeight := 1.8
-	userWeight := 100.0
+	var userHeight float64
+	var userWeight float64
+	fmt.Print("__ Калькулятор индекса массы тела __\n")
+	fmt.Print("Введите свой рост в метрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userWeight)
 	bmi := userWeight / math.Pow(userHeight, bmiPower)
+	fmt.Print("Ваш индекс массы тела: ")
 	fmt.Print(bmi)
 }
