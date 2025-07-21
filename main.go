2e4060630ba5294b9246793222b913508635e642
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-21 10:50:50 +0300

    Constants

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
+	const bmiPower = 2
 	userHeight := 1.8
 	userWeight := 100.0
-	bmi := userWeight / math.Pow(userHeight, 2)
+	bmi := userWeight / math.Pow(userHeight, bmiPower)
 	fmt.Print(bmi)
 }
