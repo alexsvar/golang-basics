e6de0e8e6bd8e9b53e6151cdce8ec294cb6d284b
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-22 15:46:40 +0300

    Infinite loop with condition

--- main.go
+++ main.go
@@ -8,31 +8,30 @@ import (
 const bmiPower = 2
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 5 {
-		 	continue
-		}
-		fmt.Printf("%d\n", i)
-	}
-
 	fmt.Println("__ Калькулятор индекса массы тела __")
-	userHeight, userWeight := getUserInput()
-	BMI := calculateBMI(userHeight, userWeight)
-	outputResult(BMI)
-	switch {
-	case BMI < 18.5:
-		fmt.Println("У вас сильный дефицит массы тела")
-	case BMI < 25: 
-		fmt.Println("У вас нормальная масса тела")
-	case BMI < 30: 
-		fmt.Println("У вас избыточная масса тела")
-	default: fmt.Println("У вас степень ожирения")
+	for {
+		userHeight, userWeight := getUserInput()
+		BMI := calculateBMI(userHeight, userWeight)
+		outputResult(BMI)
+		isRepeatCalculation := checkRepeatCalculation()
+		if !isRepeatCalculation {
+			break
+		}
 	}
 }
 
 func outputResult(bmi float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", bmi)
 	fmt.Println(result)
+	switch {
+	case bmi < 18.5:
+		fmt.Println("У вас сильный дефицит массы тела")
+	case bmi < 25: 
+		fmt.Println("У вас нормальная масса тела")
+	case bmi < 30: 
+		fmt.Println("У вас избыточная масса тела")
+	default: fmt.Println("У вас степень ожирения")
+	}
 }
 
 func calculateBMI(userHeight float64, userWeight float64) float64 {
@@ -48,4 +47,14 @@ func getUserInput() (float64, float64)  {
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userWeight)
 	return userHeight, userWeight
+}
+
+func checkRepeatCalculation() bool {
+	var userChoice string
+	fmt.Print("Вы хотите сделать ещё один расчёт? (y/n): ")
+	fmt.Scan(&userChoice)
+	if userChoice == "y" || userChoice == "Y" {
+		return true
+	}
+	return false
 }
