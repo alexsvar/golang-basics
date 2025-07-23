87b728490fa86ac090b20e29a8ec73a57ae80d8e
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-23 20:28:19 +0300

    Changing & iteration the map

--- go-demo-3/main.go
+++ go-demo-3/main.go
@@ -5,6 +5,23 @@ import "fmt"
 func main() {
 	m := map[string]string{
 		"Github": "https://github.com",
+		"Google": "https://google.com",
+		"Yandex": "yandex",
 	}
 	fmt.Println(m)
+	fmt.Println(m["Github"])
+
+	m["Yandex"] = "https://yandex.ru"
+	fmt.Println(m["Yandex"])
+	
+	m["VK"] = "https://vk.com"
+	fmt.Println(m["VK"])
+	
+	delete(m, "VK")
+	fmt.Println(m)
+
+
+	for key, value := range m {
+		fmt.Println(key, value)
+	}
 }
