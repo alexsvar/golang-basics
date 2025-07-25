21004c96fadba17365e78b21191f8b4d7b244c45
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-25 13:32:34 +0300

    Exercise struct mutation

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -11,27 +11,36 @@ type account struct {
 	url string
 }
 
-func (acc account) outputPassword() {
+func (acc *account) outputPassword() {
 	fmt.Println(acc)
 	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	result := make([]rune, n)
+	for i := range result {
+		result[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(result)
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_!?*")
 
 func main() {
-	fmt.Println(generatePassword(12))
+
 
 	login := promptData("Введите логин: ")
-	password := promptData("Введите пароль: ")
+	// password := promptData("Введите пароль: ")
 	url := promptData("Введите URL: ")
 
 	myAccount := account{
 		login: login,
-		password: password,
 		url: url,
 	}
 
+	myAccount.generatePassword(12)
 	myAccount.outputPassword()
+	fmt.Println(myAccount)
 }
 
 func promptData(prompt string) string {
@@ -39,13 +48,4 @@ func promptData(prompt string) string {
 	var result string
 	fmt.Scan(&result)
 	return result
-}
-
-
-func generatePassword(n int) string {
-	result := make([]rune, n)
-	for i := range result {
-		result[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	return string(result)
 }
