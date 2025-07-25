7dad0e5e690dbf2bbdd9c390c95f4c00e5fd3ba1
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-25 15:49:49 +0300

    Struct composition

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -5,12 +5,19 @@ import (
 	"fmt"
 	"math/rand/v2"
 	"net/url"
+	"time"
 )
 
 type account struct {
 	login string
 	password string
 	url string
+}
+
+type accountWithTimestamp struct{
+	createdAt time.Time
+	updatedAt time.Time
+	acc account
 }
 
 func (acc *account) outputPassword() {
@@ -28,7 +35,28 @@ func (acc *account) generatePassword(n int) {
 
 // 1. !login -> error
 // 2. !password -> generatePassword()
-func newAccount(login, password, urlString string) (*account, error) {
+// func newAccount(login, password, urlString string) (*account, error) {
+// 	if login == "" {
+// 		return nil, errors.New("INVALID_LOGIN")
+// 	}
+// 	// Validation
+// 	_, err := url.ParseRequestURI(urlString)
+// 	if err != nil {
+// 		return nil, errors.New("INVALID_URL")
+// 	}
+
+// 	newAcc := &account{
+// 		login: login,
+// 		password: password,
+// 		url: urlString,
+// 	}
+// 	if password == "" {
+// 		newAcc.generatePassword(12)
+// 	}
+// 	return newAcc, nil
+// }
+
+func newAccountWithTimestamp(login, password, urlString string) (*accountWithTimestamp, error) {
 	if login == "" {
 		return nil, errors.New("INVALID_LOGIN")
 	}
@@ -38,13 +66,17 @@ func newAccount(login, password, urlString string) (*account, error) {
 		return nil, errors.New("INVALID_URL")
 	}
 
-	newAcc := &account{
-		login: login,
-		password: password,
-		url: urlString,
+	newAcc := &accountWithTimestamp{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		acc: account{
+			login: login,
+			password: password,
+			url: urlString,
+		},
 	}
 	if password == "" {
-		newAcc.generatePassword(12)
+		newAcc.acc.generatePassword(12)
 	}
 	return newAcc, nil
 }
@@ -56,13 +88,13 @@ func main() {
 	password := promptData("Введите пароль: ")
 	url := promptData("Введите URL: ")
 
-	myAccount, err := newAccount(login, password, url)
+	myAccount, err := newAccountWithTimestamp(login, password, url)
 	if err != nil {
 		fmt.Println("Неверный формат URL или Login")
 		return
 	}
 
-	myAccount.outputPassword()
+	myAccount.acc.outputPassword()
 	fmt.Println(myAccount)
 }
 
