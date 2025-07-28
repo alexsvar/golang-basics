263a0c8e5af6e1b1508f38959a8652903b9e4513
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-28 11:27:47 +0300

    Splitting the code into packages

---
+++ go-demo-4/account.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"time"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_!?*")
+
+type account struct {
+	login string
+	password string
+	url string
+}
+
+type accountWithTimestamp struct{
+	createdAt time.Time
+	updatedAt time.Time
+	acc account
+}
+
+func (acc *account) outputPassword() {
+	fmt.Println(acc)
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	result := make([]rune, n)
+	for i := range result {
+		result[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(result)
+}
+
+func newAccountWithTimestamp(login, password, urlString string) (*accountWithTimestamp, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
+	// Validation
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID_URL")
+	}
+
+	newAcc := &accountWithTimestamp{
+		createdAt: time.Now(),
+		updatedAt: time.Now(),
+		acc: account{
+			login: login,
+			password: password,
+			url: urlString,
+		},
+	}
+	if password == "" {
+		newAcc.acc.generatePassword(12)
+	}
+	return newAcc, nil
+}
--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -1,87 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
-	"time"
 )
 
-type account struct {
-	login string
-	password string
-	url string
-}
 
-type accountWithTimestamp struct{
-	createdAt time.Time
-	updatedAt time.Time
-	acc account
-}
-
-func (acc *account) outputPassword() {
-	fmt.Println(acc)
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatePassword(n int) {
-	result := make([]rune, n)
-	for i := range result {
-		result[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	acc.password = string(result)
-}
-
-// 1. !login -> error
-// 2. !password -> generatePassword()
-// func newAccount(login, password, urlString string) (*account, error) {
-// 	if login == "" {
-// 		return nil, errors.New("INVALID_LOGIN")
-// 	}
-// 	// Validation
-// 	_, err := url.ParseRequestURI(urlString)
-// 	if err != nil {
-// 		return nil, errors.New("INVALID_URL")
-// 	}
-
-// 	newAcc := &account{
-// 		login: login,
-// 		password: password,
-// 		url: urlString,
-// 	}
-// 	if password == "" {
-// 		newAcc.generatePassword(12)
-// 	}
-// 	return newAcc, nil
-// }
-
-func newAccountWithTimestamp(login, password, urlString string) (*accountWithTimestamp, error) {
-	if login == "" {
-		return nil, errors.New("INVALID_LOGIN")
-	}
-	// Validation
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, errors.New("INVALID_URL")
-	}
-
-	newAcc := &accountWithTimestamp{
-		createdAt: time.Now(),
-		updatedAt: time.Now(),
-		acc: account{
-			login: login,
-			password: password,
-			url: urlString,
-		},
-	}
-	if password == "" {
-		newAcc.acc.generatePassword(12)
-	}
-	return newAcc, nil
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_!?*")
 
 func main() {
 	login := promptData("Введите логин: ")
