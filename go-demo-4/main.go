ea3388d5817e8ae70ca019b8314efc7902291c43
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-30 14:46:54 +0300

    Added menu

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -7,8 +7,37 @@ import (
 )
 
 func main() {
+	// 1. create account
+	// 2. find account
+	// 3. delete account
+	// 4. exit
+	fmt.Println("___МЕНЕДЖЕР ПАРОЛЕЙ___")
+	for {
+		variant := getMenu()
+		Menu:
+		switch variant {
+		case 1:
+			createAccount()
+		case 2:
+			findAccount()
+		case 3:
+			deleteAccount()
+		default:
+			break Menu;
+		}
+	}
 	createAccount()
-	
+}
+
+func getMenu() int {
+	var variant int
+	fmt.Println("Выберите вариант:")
+	fmt.Println("1 - Создать аккаунт")
+	fmt.Println("2 - Найти аккаунт")
+	fmt.Println("3 - Удалить аккаунт")
+	fmt.Println("4 - Выход")
+	fmt.Scan(&variant)
+	return variant
 }
 
 func createAccount() {
@@ -28,6 +57,10 @@ func createAccount() {
 	}
 	files.WriteFile(file, "data.json")
 }
+
+func findAccount() {}
+
+func deleteAccount() {}
 
 func promptData(prompt string) string {
 	fmt.Print(prompt)
