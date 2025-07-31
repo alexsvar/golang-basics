f35ad9b61754cc5b9a78c75f31fc756565cc22b9
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-07-31 08:42:02 +0300

    Updated deleting account by url

--- go-demo-4/account/vault.go
+++ go-demo-4/account/vault.go
@@ -38,12 +38,7 @@ func NewVault() *Vault {
 // Добавление аккаунта с преобразованием
 func (vault *Vault) AddAccount(acc Account) {
 	vault.Accounts = append(vault.Accounts, acc)
-	vault.UpdatedAt = time.Now()
-	data, err := vault.ToBytes()
-	if err != nil {
-		fmt.Println("Не удалось преобразовать")
-	}
-	files.WriteFile(data, "data.json")
+	vault.save()
 }
 
 // Преобразование
@@ -74,17 +69,22 @@ func (vault *Vault) DeleteAccountByUrl(url string) bool {
 	for _, account := range vault.Accounts {
 		isMatched := strings.Contains(account.Url, url)
 		if !isMatched {
+			// vault.Accounts = append(vault.Accounts[:i], vault.Accounts[i+1]...)
 			accounts = append(accounts, account)
 			continue
 		}
 		isDeleted = true
 	}
 	vault.Accounts = accounts
+	vault.save()
+	return isDeleted
+}
+
+func (vault *Vault) save() {
 	vault.UpdatedAt = time.Now()
 	data, err := vault.ToBytes()
 	if err != nil {
 		fmt.Println("Не удалось преобразовать")
 	}
 	files.WriteFile(data, "data.json")
-	return isDeleted
 }
